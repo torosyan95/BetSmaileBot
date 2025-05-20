1d4a6a6344d7dac9d8f89066bc8a2934b6320933
Karen <84942855+torosyan95@users.noreply.github.com>
2025-05-20 13:14:17 +0400

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CRYPTOCLOUD_API_KEY = os.getenv("CRYPTOCLOUD_API_KEY")
-CRYPTOCLOUD_SHOP_ID = os.getenv("CRYPTOCLOUD_SHOP_ID")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
